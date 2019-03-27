99d0b46246aa6ff0ce4ea5079f1bad1edb207a19
Sean Tayler <seanjtayler@gmail.com>
2019-03-27 15:19:51 -0700

    installed axios and setup config

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBBl3_QtZYPXzx-RqcESp7GXx7XgY1K1G0';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+})
